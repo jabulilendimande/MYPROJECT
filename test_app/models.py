d8747dc467ec10e82b46e22abf696d7e1ce42457
Jabulile Ndimande <131398578+jabulilendimande@users.noreply.github.com>
2025-06-24 11:01:27 +0200

    adding models

--- test_app/models.py
+++ test_app/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here. This is the data I want to show on website from the database
+class Test_app(models.Model):
+    firstname = models.CharField(max_length=15)
+    lastname = models.CharField(max_length=15)
